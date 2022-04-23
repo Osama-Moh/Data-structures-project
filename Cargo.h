af40aa2b61e68ec3c9446cecc8abca9421a7e4a4
YousefEmad2002 <103539314+YousefEmad2002@users.noreply.github.com>
2022-04-23 15:33:47 +0200

    Cargo Update

--- Cargo.h
+++ Cargo.h
@@ -1,20 +1,34 @@
 #pragma once
-# include <iostream>
-using namespace std;
-# include <string>
 
-class cargo
+
+class Cargo
 {
 private:
-	int preparetime;
-	float loadtime;
-	string cargotype;
-	float cost;
-	int distance;
+	char TYP;
+	int ID;
+	int PTD;
+	int PTH;
+	int LT;
+	int DIST;
+	int COST;
 
 public:
-
-
-
+	Cargo();
+	Cargo(char typ, int id, int ptd, int pth, int lt, int dist, int cost);
+	void setTYP(char typ);
+	void setID(int id);
+	void setPTD(int ptd);
+	void setPTH(int pth);
+	void setLT(int lt);
+	void setDIST(int dist);
+	void setCOST(int cost);
+	char getTYP();
+	int getID();
+	int getPTD();
+	int getPTH();
+	int getLT();
+	int getDIST();
+	int getCOST();
+	~Cargo();
 
 };
