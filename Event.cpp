ee2c5621e3a5586252fc5f0c0eae254fefd36aac
YousefEmad2002 <yousefemadibraheem@gmail.com>
2022-04-24 17:42:06 +0200

    Quick Update

--- Event.cpp
+++ Event.cpp
@@ -1,11 +1,48 @@
 #include "Event.h"
 
 
-Event::Event(void)
+Event::Event()
 {
 }
-
-
-Event::~Event(void)
+Event::Event(char type, int day, int hour, int id)
+{
+	setType(type);
+	setDay(day);
+	setHour(hour);
+	setID(id);
+}
+void Event::setType(char type)
+{
+	Type=type;
+}
+void Event::setDay(int day)
+{
+	Day=day;
+}
+void Event::setHour(int hour)
+{
+	Hour=hour;
+}
+void Event::setID(int id)
+{
+	ID=id;
+}
+char Event::getType()
+{
+	return Type;
+}
+int Event::getDay()
+{
+	return Day;
+}
+int Event::getHour()
+{
+	return Hour;
+}
+int Event::getID()
+{
+	return ID;
+}
+Event::~Event()
 {
 }
--- Event.h
+++ Event.h
@@ -5,10 +5,22 @@
 class Event
 {
 private:
+	char Type;
+	int Day;
+	int Hour;
+	int ID;
 public:
 	Event();
-	Event(int ID);
+	Event(char type, int day, int hour, int id);
 	virtual void Execute(LinkedQueue<Cargo*> Q) = 0;
+	void setType(char type);
+	void setDay(int day);
+	void setHour(int hour);
+	void setID(int id);
+	char getType();
+	int getDay();
+	int getHour();
+	int getID();
 	virtual ~Event();
 };
 
