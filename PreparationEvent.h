e9d92f7b8a30692ac32cb8735b64cc6564a0491a
YousefEmad2002 <103539314+YousefEmad2002@users.noreply.github.com>
2022-04-25 17:18:58 +0200

    Major Update

--- PreparationEvent.h
+++ PreparationEvent.h
@@ -5,13 +5,22 @@
 class PreparationEvent: public Event
 {
 private:
+	char Type;
 	int Dist;
 	int Load;
 	int Cost;
 public:
 	PreparationEvent();
-	PreparationEvent(char type, int id, int day, int hour, int load, int dist, int cost);
+	PreparationEvent(int id, int day, int hour, char type, int load, int dist, int cost);
 	void Execute(Company* C);
+	void setType(char type);
+	void setLoad(int load);
+	void setDist(int dist);
+	void setCost(int cost);
+	char getType();
+	int getLoad();
+	int getDist();
+	int getCost();
 	~PreparationEvent();
 };
 
